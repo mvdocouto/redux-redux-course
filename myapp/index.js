37b26db3fa84393bf72190b69596dc415e60ce21
Marcus Couto <markitovc@gmail.com>
2019-03-06 19:27:36 -0300

    Insert editing note

--- myapp/index.js
+++ myapp/index.js
@@ -8,9 +8,9 @@ import "./index.scss";
 class App extends React.Component {
   state = {
     notes: [
-      { id: uuid(), text: "Estudar React" },
-      { id: uuid(), text: "Estudar Redux" },
-      { id: uuid(), text: "Estudar Saga" }
+      { id: 1, text: "Estudar React" },
+      { id: 2, text: "Estudar Redux" },
+      { id: 3, text: "Estudar Saga" }
     ]
   };
 
@@ -46,7 +46,17 @@ class App extends React.Component {
     });
   };
 
-  handleEdit = () => {};
+  handleEdit = (id, text) => {
+    this.setState(prevState => {
+      const newNotes = prevState.notes.slice();
+      const index = newNotes.findIndex(note => note.id === id);
+      newNotes[index].text = text;
+
+      return {
+        notes: newNotes
+      };
+    });
+  };
 
   render() {
     return (
@@ -96,50 +106,125 @@ class NewNote extends React.Component {
   }
 }
 
+class Note extends React.Component {
+  state = {
+    isEditing: false
+  };
+
+  handleEdit = () => {
+    this.setState({
+      isEditing: true
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      isEditing: false
+    });
+  };
+
+  handleSave = () => {
+    this.setState({
+      isEditing: false
+    });
+    this.props.onEdit(this.props.note.id, this.input.value);
+  };
+
+  render() {
+    const { note, onEdit, onDelete, onMove, index, count } = this.props;
+    const { isEditing } = this.state;
+    return (
+      <div className="note">
+        {isEditing ? (
+          <input
+            className="note__input"
+            type="text"
+            defaultValue={note.text}
+            ref={c => {
+              this.input = c;
+            }}
+          />
+        ) : (
+          <span className="note__text">{note.text}</span>
+        )}
+
+        {isEditing ? (
+          <React.Fragment>
+            <button
+              className={"note__button--red"}
+              onClick={() => {
+                this.handleCancel();
+              }}
+            >
+              <i className="material-icons">cancel</i>
+            </button>
+            <button
+              className={"note__button--green"}
+              onClick={() => {
+                this.handleSave();
+              }}
+            >
+              <i className="material-icons">done_outline</i>
+            </button>
+          </React.Fragment>
+        ) : null}
+
+        <button
+          disabled={isEditing}
+          className={"note__button"}
+          onClick={event => {
+            this.handleEdit();
+          }}
+        >
+          <i className="material-icons">edit</i>
+        </button>
+        <button
+          disabled={isEditing}
+          className={"note__button"}
+          onClick={() => {
+            onDelete(note.id);
+          }}
+        >
+          <i className="material-icons">delete</i>
+        </button>
+        <button
+          className={classNames("note__button", {
+            "note__button--hidden": index === 0
+          })}
+          onClick={() => {
+            onMove("up", index);
+          }}
+        >
+          <i className="material-icons">arrow_upward</i>
+        </button>
+        <button
+          className={classNames("note__button", {
+            "note__button--hidden": index === count - 1
+          })}
+          onClick={() => {
+            onMove("down", index);
+          }}
+        >
+          <i className="material-icons">arrow_downward</i>
+        </button>
+      </div>
+    );
+  }
+}
+
 const NoteList = ({ notes, onMove, onDelete, onEdit }) => {
   return (
     <div className="note-list">
       {notes.map((note, index) => (
-        <div key={note.id} className="note">
-          <span className="note__text">{note.text}</span>
-          <button
-            className={"note__button"}
-            onClick={() => {
-              onEdit(note.id);
-            }}
-          >
-            <i className="material-icons">edit</i>
-          </button>
-          <button
-            className={"note__button"}
-            onClick={() => {
-              onDelete(note.id);
-            }}
-          >
-            <i className="material-icons">delete</i>
-          </button>
-
-          <button
-            className={classNames("note__button", {
-              "note__button--hidden": index === 0
-            })}
-            onClick={() => {
-              onMove("up", index);
-            }}
-          >
-            <i className="material-icons">arrow_upward</i>
-          </button>
-          <button
-            className={classNames("note__button", {
-              "note__button--hidden": index === notes.length - 1
-            })}
-            onClick={() => {
-              onMove("down", index);
-            }}
-          >
-            <i className="material-icons">arrow_downward</i>
-          </button>
-        </div>
+        <Note
+          key={note.id}
+          onMove={onMove}
+          onDelete={onDelete}
+          onEdit={onEdit}
+          note={note}
+          index={index}
+          count={notes.length}
+        />
       ))}
     </div>
   );
